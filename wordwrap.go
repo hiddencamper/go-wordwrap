415e5b6920eab9d388fcd909d8a596d8612457a5
Hiddencamper <Hiddencamper@gmail.com>
2024-10-26 22:39:37 -0500

    Added documentation

--- wordwrap.go
+++ wordwrap.go
@@ -4,6 +4,19 @@ import (
 	"strings"
 )
 
+// WordWrap takes a string and wraps it to fit within a certain width.
+//
+// All instances of " " are replaced with newlines (\n) to wrap the text.
+// If newlines (\n) are in the text, it will split the string and continue.
+//
+// The algorithm works by looping through the string and finding the last
+// index of a space that is not beyond the width. If it finds a newline
+// character before the last space, it will use the newline character
+// instead. If it doesn't find a space or a newline, it will split the
+// string at the width.
+//
+// The function will return a single string with all the sub-strings
+// joined together with newlines (\n) in between.
 func WordWrap(s string, w int) string {
 	var out []string
 
