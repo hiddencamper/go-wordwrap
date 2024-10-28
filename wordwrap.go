bbc5cf9a16ae7d7484e54906fb13a1cf5b86486c
Hiddencamper <Hiddencamper@gmail.com>
2024-10-27 21:02:26 -0500

    Corrected special cases where a string starts with \n, where a space is the last character of a width block. improved newline algorithm.

--- wordwrap.go
+++ wordwrap.go
@@ -19,25 +19,26 @@ import (
 // joined together with newlines (\n) in between.
 func WordWrap(s string, w int) string {
 	var out []string
-
 	for len(s) > w {
-		lastIndex := strings.LastIndex(s[:w], " ")
 		newlineIndex := strings.LastIndex(s[:w], "\n")
-		if newlineIndex < lastIndex && newlineIndex > 0 {
+		if newlineIndex >= 0 {
 			out = append(out, s[:newlineIndex])
 			s = s[newlineIndex+1:]
 			continue
 		}
-		if lastIndex == -1 {
-			lastIndex = w
-		}
-
-		out = append(out, s[:lastIndex])
-		if lastIndex <= w {
+		lastIndex := strings.LastIndex(s[:w], " ")
+		if lastIndex == 0 {
 			s = s[lastIndex+1:]
 			continue
 		}
-		s = s[lastIndex:]
+		if lastIndex == -1 {
+			lastIndex = w
+			out = append(out, s[:lastIndex])
+			s = s[lastIndex:]
+			continue
+		}
+		out = append(out, s[:lastIndex])
+		s = s[lastIndex+1:]
 	}
 	out = append(out, s)
 
--- wordwrap_test.go
+++ wordwrap_test.go
@@ -11,4 +11,7 @@ func TestWordWrap(t *testing.T) {
 	s = "\nSpaceTraders is a headless API and fleet-management game\nwhere players can work together or against each other to trade, explore, expand, and conquer in a dynamic and growing universe. Build your own UI, write automated scripts, or just play the game from the comfort of your terminal. The game is currently in alpha and is under active development."
 	s = WordWrap(s, 80)
 	t.Log(s)
+	s = "\nabcde fghjiklmnopqrstuvwsyz"
+	s = WordWrap(s, 5)
+	t.Log(s)
 }
